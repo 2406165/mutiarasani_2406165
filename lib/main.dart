a18dd353644f5f3d82c285387c24c5a96539e9a4
Your Name <you@example.com>
2025-04-28 20:21:41 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:tugas_1_app/views/number_nine_screen.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'latihan slicing ul',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white10),
+          useMaterial3: false,
+        ),
+        home: NumberNineScreen());
+  }
+}
